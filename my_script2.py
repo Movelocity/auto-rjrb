d4e2a6a4f9272aecca16befadea196d58d67fd41
Locity <55646600+Movelocity@users.noreply.github.com>
2021-11-09 09:30:42 +0800

    Update my_script2.py

--- my_script2.py
+++ my_script2.py
@@ -10,18 +10,7 @@ def send_mes():
   my_sender = os.environ.get("SENDER")
   my_pass = os.environ.get("MIYAO")
   my_user = os.environ.get("TARGET_MAIL")
-  print('target mail: ', my_user)
-  try:
-    msg = MIMEText('hello', 'plain', 'utf-8')  # 填写邮件内容
-    msg['From'] = formataddr(["in the campus", my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
-    msg['To'] = formataddr(["209848539@qq.com", my_user])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
-    msg['Subject'] = 'OK'  # 邮件的主题，也可以说是标题
-
-    server = smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器
-    server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱授权码
-    server.sendmail(my_sender, [my_user, ], msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
-    server.quit()  # 关闭连接
-  except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
-    print(Exception)
+  print('target mail: ', my_user, "; sender: ", my_sender, "; pass: ", my_pass)
+  
 
 send_mes()
