75802d2f8b6396aef2b31a501b77fcffdd238390
Locity <55646600+Movelocity@users.noreply.github.com>
2021-11-09 20:47:19 +0800

    Update one_mail.py

--- one_mail.py
+++ one_mail.py
@@ -10,6 +10,7 @@ def send_mes():
   #print('target mail len: ', len(reciever), "; sender: ", sender, "; pass: ", key) #you can see these args show like *** cause they are hidden
   try:
     content = 'Have a nice day.\\n\tClock: '+str(datetime.datetime.now().hour+8)
+    print(content)
     msg = MIMEText(content, 'plain', 'utf-8')
     msg['From'] = formataddr(["GithubAction", sender])
     msg['To'] = formataddr(["Locity", reciever])
