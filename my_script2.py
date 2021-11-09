75a03cecfafe1fe745b8ffa0d970f113adeaa304
Locity <55646600+Movelocity@users.noreply.github.com>
2021-11-09 13:24:14 +0800

    Update my_script2.py

--- my_script2.py
+++ my_script2.py
@@ -3,14 +3,29 @@ import os
 import smtplib
 from email.mime.text import MIMEText
 from email.utils import formataddr
+import datetime
 
 print('system version:', sys.version)
 
 def send_mes():
-  my_sender = os.environ.get("SENDER")
-  my_pass = os.environ.get("MIYAO")
-  my_user = os.environ.get("TARGET")
-  print('target mail len: ', len(my_user), "; sender: ", my_sender, "; pass: ", my_pass)
-  
+  sender = os.environ.get("SENDER")
+  key = os.environ.get("MIYAO")
+  reciever = os.environ.get("TARGET")
+  #print('target mail len: ', len(reciever), "; sender: ", sender, "; pass: ", key) #you can see these args show like *** cause they are hidden
+  try:
+    content = 'Have a nice day.\n\tClock: '+str(datetime.datetime.now().hour)
+    msg = MIMEText(content, 'plain', 'utf-8')
+    msg['From'] = formataddr(["GithubAction", sender])
+    msg['To'] = formataddr(["Locity", reciever])
+    msg['Subject'] = 'The title was eaten'
+    # message body formed
+    server = smtplib.SMTP_SSL("smtp.qq.com", 465)
+    server.login(sender, key)
+    server.sendmail(sender, [reciever, ], msg.as_string())
+    server.quit()
+  except Exception:
+    print(Exception)
+    return false
+  return true
 
 send_mes()
