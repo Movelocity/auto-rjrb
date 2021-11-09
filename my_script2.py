fe7566572d71e23c7deeb7dac871b4e47030254c
Locity <55646600+Movelocity@users.noreply.github.com>
2021-11-09 13:01:16 +0800

    Update my_script2.py

--- my_script2.py
+++ my_script2.py
@@ -9,8 +9,8 @@ print('system version:', sys.version)
 def send_mes():
   my_sender = os.environ.get("SENDER")
   my_pass = os.environ.get("MIYAO")
-  my_user = os.environ.get("TARGET_MAIL")
-  print('target mail: ', my_user, "; sender: ", my_sender, "; pass: ", my_pass)
+  my_user = os.environ.get("TARGET")
+  print('target mail: ', my_user[:], "; sender: ", my_sender, "; pass: ", my_pass)
   
 
 send_mes()
